ce5187bf7ef217d19d2aa40a1255e1bf7900f5e7
hyosang <a4427777@naver.com>
2015-11-10 13:52:44 +0900

    added main code file 		by hskim92

---
+++ TP1.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#define TOTALFORK 3
+
+int main(int argc, char* argv){
+
+	pid_t pids[TOTALFORK], pid;
+	int runProcess = 0;
+	int state;
+
+	while(runProcess < TOTALFORK){
+		pids[runProcess] = fork();
+		if(pids[runProcess] < 0){
+			return -1;
+		}
+		else if(pids[runProcess] == 0){
+			int i =0;
+			for(i=0; i<5; i++){
+				printf("child process %ld : number(%d) \n", (long)getpid(), i);
+			}
+			exit(0);
+		}else{
+			printf("parent %ld, chile %ld\n", (long)getpid(),
+				(long)pids[runProcess]);
+		}
+		runProcess++;
+	}
+	return 0;
+}
