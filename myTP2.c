902e0790ca044217c3d3f6be54a5836237374fc5
Woojeong <woojong610@gmail.com>
2015-11-15 20:59:34 +0900

    kill

--- myTP2.c
+++ myTP2.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <signal.h>
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/msg.h>
@@ -9,7 +11,7 @@
 #define TOTALFORK 10
 #define READY 0
 #define WAIT 1
-#define QUANTUM 20
+#define QUANTUM 5
 
 typedef struct Process{
 	pid_t pid;
@@ -21,7 +23,7 @@ typedef struct Process{
 Proc* createProc(pid_t newPid){
 	Proc* p = (Proc*)malloc(sizeof(Proc));
 	p->pid = newPid;
-	p->cpu_time = rand()%50;
+	p->cpu_time = rand()%10;
 	p->io_time = rand()%3;
 	p-> state = READY;
 	return p;
@@ -41,19 +43,6 @@ typedef struct Queue{
 
 	int size;
 } Queue;
-
-/*
-Proc* initProc(pid_t newPid){
-	Proc* newProc = (Proc*)malloc(sizeof(Proc));
-
-	newProc->pid = newPid;
-	newProc->cpu_time = rand()%50;
-	newProc->io_time = rand()%3;
-	newProc->state = READY;
-
-	return newProc;
-}
-*/
 
 void enqueue(Queue* q, Proc* newItem);
 
@@ -99,10 +88,10 @@ void printQueue(Queue* q){
 typedef struct msgbuf{
 	long msg_type;
 	char msg_text;
-	int seq;
 } msg_buf;
 
-int main(int argc, char* argv){
+
+int main(){
 	//msg queue var
 	key_t key_id;
 	int i;
@@ -112,6 +101,8 @@ int main(int argc, char* argv){
 	int runningProc = 0;
 	pid_t pid;
 	Proc* currentProc;
+	
+	int tick = 0;
 
 	Queue* runQ = createQueue();
 	Queue* waitQ = createQueue();
@@ -132,18 +123,22 @@ int main(int argc, char* argv){
 		
 		if(pid < 0) return -1;
 		else if(pid == 0){
-			for(i = 0; i < QUANTUM; i++, currentProc->cpu_time--){
-				printf("child process %ld : time quantum (%d), remaining cpu time (%d)\n", (long)getpid(), i, currentProc->cpu_time);
+			for(i = 0; i < QUANTUM; i++, currentProc->cpu_time--){		
+				printf("tick(%d) : child process %ld : time quantum (%d), remaining cpu time (%d)\n", tick, (long)getpid(), i, currentProc->cpu_time);
 
-			
-			
+				if(currentProc->cpu_time == 0){
+					kill(currentProc->pid, SIGKILL);
+				}
+				
 				printf("run Queue : ");
 				printQueue(runQ);
 				printf("wait Queue : ");
 				printQueue(waitQ);
 				printf("retire Queue : ");
 				printQueue(retireQ);	
-				printf("\n");		
+				printf("\n");	
+
+				tick++;	
 
 			}
 
